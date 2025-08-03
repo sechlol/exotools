cbbb9ff00c3c60cf34771f7e42b668ce1d74626f
Christian Cardin <ccardin.sech@gmail.com>
2025-08-03 16:05:22 +0300

    iterative sync queries in tapservice

--- exotools/downloaders/tap_service.py
+++ exotools/downloaders/tap_service.py
@@ -1,17 +1,20 @@
-from typing import Optional
+from typing import Optional, Iterable, Iterator
 
 import pyvo as vo
-from astropy.table import QTable
+from astropy.table import QTable, vstack
 from pyvo.dal.vosi import VOSITables
+from tqdm import tqdm
 
 from exotools.utils.qtable_utils import QTableHeader, TableColumnInfo
 
 
 class TapService:
     """
-    Unfortunately, this method is not suitable for large queries, as it doesn't support authentication.
-    Use casjobs instead for large queries (see download_dataset.query_ctl_casjob())
+    Unfortunately, this service is not suitable for large queries, as it doesn't support authentication.
+    Use 'casjobs' package instead for large queries
     """
+
+    MAX_CHUNK_SIZE = 2000
 
     def __init__(self, url: str):
         self._url = url
@@ -52,12 +55,45 @@ class TapService:
             raise KeyError(f"{table_name} not in TapService {self._url}")
         return [c.name for c in tables[table_name].columns]
 
-    def query(self, query_string: str, sync: bool = False) -> QTable:
-        if sync:
-            result = self._service.run_sync(query_string)
-        else:
-            result = self._service.run_async(query_string)
+    def query(self, query_string: str) -> QTable:
+        result = self._service.run_sync(query_string)
         return result.to_qtable()
+
+    def query_chunks_iterative(
+        self,
+        select_fields: str | Iterable[str],
+        from_clause: str,
+        where_clause: str,
+        order_by: str,
+        limit: Optional[int] = None,
+    ) -> Iterator[QTable]:
+        if not limit:
+            # get total number of records
+            count_query = f"select count(*) {from_clause} {where_clause}"
+            total_records = self.query(count_query).values()
+        else:
+            total_records = limit
+
+        fields = select_fields if isinstance(select_fields, str) else ",".join(select_fields)
+        query = f"select top {self.MAX_CHUNK_SIZE} {fields} {from_clause} {where_clause} order by {order_by} asc"
+        query += " offset {offset}"
+
+        total_chunks = (total_records + self.MAX_CHUNK_SIZE - 1) // self.MAX_CHUNK_SIZE
+        for offset in tqdm(range(0, total_records, self.MAX_CHUNK_SIZE), total=total_chunks, desc="Downloading chunks"):
+            fff = query.format(offset=offset)
+            print(fff)
+            yield self.query(query.format(offset=offset))
+
+    def query_chunks(
+        self,
+        select_fields: str | Iterable[str],
+        from_clause: str,
+        where_clause: str,
+        order_by: str,
+        limit: Optional[int] = None,
+    ) -> QTable:
+        iterator = self.query_chunks_iterative(select_fields, from_clause, where_clause, order_by, limit)
+        return vstack(list(iterator))
 
     @staticmethod
     def _get_fields_info(table) -> QTableHeader:
