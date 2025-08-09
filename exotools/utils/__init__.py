34ad6568ae3e0ec9881c8cd529912040d8f55a0f
Christian Cardin <ccardin.sech@gmail.com>
2025-08-09 14:00:33 +0300

    fix utils imports

--- exotools/utils/__init__.py
+++ exotools/utils/__init__.py
@@ -1,6 +1,4 @@
-__all__ = [
-    "DownloadParams",
-    "TimeInfo",
-    "TableColumnInfo",
-    "QTableHeader",
-]
+from .download import DownloadParams
+from .qtable_utils import QTableHeader, TableColumnInfo
+
+__all__ = ["DownloadParams", "TableColumnInfo", "QTableHeader"]
